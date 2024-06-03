a98168e6ea4def5c33e55810b22587b12be7b732
Nurik4409 <Nurmuhammad2220@icloud.com>
2024-06-02 20:18:51 -0700

    14 tasks are comleted

---
+++ main.py
@@ -0,0 +1,102 @@
+# 1
+# n = int(input('chislo'))
+# x = (n + n * 2)
+# print(x)
+
+# 2
+# a = 46
+# z = 'str'
+# print(a, z * 5)
+
+# 3
+# x = ('привет', 'f', 5, 90.2)
+# print(x[0])
+
+# 4
+# r = int(input('chislo'))
+# if 999<r<10000:
+#     print(r)
+# else:
+#     print(None)
+
+# 5
+# z = input('name:')
+# x = input('familiya:')
+# c = input('age:')
+# print(z, x, c)
+
+# 6
+# a = int(input('1chislo:'))
+# b = int(input('2chislo:'))
+# c = int(input('3chislo:'))
+# print("summa:", a + b + c)
+# print("raznost:", a - b - c)
+# print("proizvedeniye:", a * b * c)
+
+# 7
+# a = 5
+# b = 2.2
+# c = "3"
+# print(a * b * c)
+
+# 8
+# a = 2
+# b = 3
+# c = 4
+# print('summa:', a + b + c)
+
+# 9
+# b = 'nurik'
+# z = input('parol:')
+# a = input('vedite zanovo:')
+# if z == a == b:
+#     print('parol prinat , vxodite')
+# else:
+#     print('neverniy parol')    
+
+# 10
+# a = int(input('1chislo:'))
+# b = int(input('2chislo:'))
+# c = input('znak:')
+
+# if c == '-':
+#     print(a - b)
+# elif c == '+':
+#     print(a + b)
+# elif c == '*':
+#     print(a * b)
+# elif c == '%':
+#     print(a % b)        
+# else:
+#     print('zanovo')
+
+# 11
+# a = ('<<Hello world>>')
+# print(a)
+
+# 13
+# x = int(input('chislo'))
+# if x % 2 == 0:
+#     print('chotno')
+# elif x % 2 == 1:
+#     print('nechotno')    
+# else:
+#         print('zanovo')  
+
+# 14
+# 13 - 20 == не смог сделать
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
